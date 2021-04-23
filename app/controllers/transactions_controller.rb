d4f1fc8a8fa67c2980570dbd376950c7eaece44c
MagdalenaJasmine <magdalenacooper92@gmail.com>
2021-04-23 11:06:27 -0700

    testing first validation

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,13 +2,21 @@ class TransactionsController < ApplicationController
     def create
         ccNum = params[:payment][:ccNum]
         exp = params[:payment][:exp]
-        user = User.new(email: params[:email])
-        user.save!
-        payment = Payment.new(user: user, ccNum: ccNum, exp: exp )
-        payment.save!
-        transaction = Transaction.new(quantity: params[:qty], total: params[:total], user: user, payment: payment )
-        transaction.save!
-        p params
-    end 
+        email = params[:email]
+        quantity = params[:quantity]
+        total = params[:total]
+
+        transaction = Transaction.new(quantity: quantity, total: total)
+
+        ActiveRecord::Base.transaction do
+            user = transaction.create_user!(email: email)
+            payment = transaction.create_payment!(ccNum: ccNum, exp: exp, user: user)
+            transaction.save!
+        end
+        render json: {"id": transaction.id}, status: 201
+    rescue ActiveRecord::RecordInvalid => exception
+        puts "Oops. We tried to do an invalid operation!"
+        render json: {"error": exception.message}, status: 422
+    end
 
 end
